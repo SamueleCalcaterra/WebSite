fba8fefbbb3a5d2af430b0d73944ef3145413635
SamueleCalcaterra <104380855+SamueleCalcaterra@users.noreply.github.com>
2024-05-25 12:15:28 +0200

    update: lightmode and darkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,34 +162,53 @@ function scrollTop(){
 window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+window.onload = function() {
+    updateThemeBasedOnTime();
+};
+
+setInterval(() => {
+    updateThemeBasedOnTime();
+}, 60000);
+
+function updateThemeBasedOnTime() {
+    const currentHour = new Date().getHours();
+    const currentTheme = getCurrentTheme();
+
+    if (currentHour >= 10 && currentHour < 18) {
+        if (currentTheme === 'dark') {
+            toggleTheme();
+        }
+    } else {
+        if (currentTheme === 'light') {
+            toggleTheme();
+        }
+    }
 }
 
-// Activate / deactivate the theme manually with the button
+function toggleTheme() {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    const currentTheme = getCurrentTheme();
+    const currentIcon = getCurrentIcon();
+    localStorage.setItem('selected-theme', currentTheme);
+    localStorage.setItem('selected-icon', currentIcon);
+}
+
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+    toggleTheme();
+});
+
 
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
